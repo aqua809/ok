58b487f4f7e0bb5c2e382cb0d08a839832c117b0
DarkBoy <71411869+Darkboy-js@users.noreply.github.com>
2020-11-18 13:06:49 +0300

    Update setlogs.js

--- commands/setlogs.js
+++ commands/setlogs.js
@@ -1,30 +1,15 @@
- const Discord = require("discord.js")
+  const Discord = require("discord.js")
 const db = require("quick.db")
- const ms = require('parse-ms')
- const fs = require('fs')
+const ms = require('parse-ms')
+const fs = require('fs')
 module.exports = {
-    name: "settings",
-    description: "bypass auser for verifying",
-    run: async (client, message, args, db , prefix) => {
-
-   if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`Missing Permission **ADMINISTRATOR**`)
-   let time = await db.get(`time_${message.guild.id}`)
-  let o = ms(time)
-let stauts  = {
-    true: 'on',
-    false: 'off'
-}
-  let embed = new Discord.MessageEmbed()
-     .setAuthor(message.author.username , message.author.displayAvatarURL())
-     .setTitle(`Alts Kicking system for ${message.guild.name}`)
-     .setDescription(` 
-    \`\`\`
-    Set days: ${o.days + "d" || '< Not set >'}
-    Logs Channel ID: ${db.get(`logs_${message.guild.id}`) || '< No Set >'}
-    Alts Kicking: ${stauts[db.get(`status_${message.guild.id}`)] || 'off'}\`\`\`
-     `)
-  .setFooter(message.guild.name , message.guild.iconURL())
-  .setTimestamp()
-  message.channel.send(embed)
-
-     }}
+   name: "setlogs",
+   description: "bypass auser for verifying",
+   run: async (client, message, args, db , prefix) => {
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`Missing Permission **ADMINISTRATOR**`)
+    let channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0])
+   if(!channel) return message.channel.send(`**Mention an channel or intest channel id**`)
+   message.channel.send(`Sucssesfully setted logs to ${channel}`)
+   db.set(`logs_${message.guild.id}`, channel.id)
+    
+    }}
